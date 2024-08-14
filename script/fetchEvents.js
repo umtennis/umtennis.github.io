85caed8e07e5ba02a08e1bc9cb2eac888b89a56a
Jesse Shen <jly.shen@gmail.com>
2024-08-14 18:25:53 -0500

    remove keys

--- script/fetchEvents.js
+++ script/fetchEvents.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fs = require('fs');
 
 // Replace with your API link
-const apiUrl = 'https://api.reachcm.com/calendar/export/web/group/expanded/1101/?start_date=2024-8-13&end_date=2025-2-28&authorization=Ki4qTk0IG5HHELe5M9ZiuIgIBkGzhQ2x9ImMw%2BiuYiIyX6z6PMmIWs6%2BItj1WRCZeLYkfaLHpTLX0k%20oKD%2FCMYaQ0DOMGPXIlDo6jx2hXp5TzKapsTvqRMh5rHj3Ynfj6UVgwRMdMs8s26Y9uSFChxiX9i%2Fnv7Iu3F388PgNyUMc4rK%20%2BmZYNQCWxOVokxAya3sdQJwpAKAAkXlnNvGDQzEbyEDCY0IoGAqIZjDfigwQgZ%2F2%2FsQ7Gk5SYDZ7KyD9nPX341Y20%20nZlcZLZcWfRgwsyhg2wlXOKuAs0YswPfAsseCT3wWZSYZFm097yAy%2B%2FdLPSSBgXgauwN41iJQCI%2FLzXW736I9RzIHjmO%20Ywxqy4ITC6z0%3D&timezone=America/Winnipeg'; // Assuming the link returns JSON
+const apiUrl = process.env.REACT_APP_CLUB_SCHEDULE_API_KEY; // Assuming the link returns JSON
 
 
 // Function to fetch JSON and extract specific event data for "Tennis Club"
